9ac65cfc8b25c718f026026abaab07faf87cd271
jlk.devops <jlk.devops@gmail.com>
2026-05-05 12:07:15 +0530

    Updated on 5th May, Updated with new form and n8n integration in javascript

--- script.js
+++ script.js
@@ -668,10 +668,10 @@ if (amrothForm) {
         const data = Object.fromEntries(new FormData(form));
         
         console.log('Form data to send:', data);
-        console.log('Webhook URL:', 'https://n8n.amroth.life/webhook/2088b984-608d-4ba3-81a3-9e7c135ef0e7');
+        console.log('Webhook URL:', 'https://n8n.amroth.life/webhook/fb5af8a0-2551-4c0f-a992-9258a1d9ec4c');
         
         try {
-            const res = await fetch('https://n8n.amroth.life/webhook/2088b984-608d-4ba3-81a3-9e7c135ef0e7', {
+            const res = await fetch('https://n8n.amroth.life/webhook/fb5af8a0-2551-4c0f-a992-9258a1d9ec4c', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
